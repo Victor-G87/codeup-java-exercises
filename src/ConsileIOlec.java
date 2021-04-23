3ee8076fe1b2f5eb494eb55b20ad720d5751fb20
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-04-23 17:12:32 -0500

    friday

---
+++ src/ConsileIOlec.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class ConsileIOlec {
+    public static void main(String[] args){
+        System.out.println("Console IO Lecture");
+
+
+//        System.out.println("here");
+//        System.out.println("there");
+
+//        System.out.print("here");
+//        System.out.print("there");
+
+        String firstName = "Douglas";
+        String lastName = "Hirsh";
+//        System.out.println(firstName + " " +lastName);
+
+        System.out.printf("Hello, %s!\n" , firstName);
+
+        int numberOfPets = 3;
+        String typeOfPets = "cats";
+        System.out.printf("I have %d %s.\n", numberOfPets, typeOfPets);
+
+
+        float currencyPennies = 1000.001F;
+        System.out.printf("My iPhone cost $%.2f", currencyPennies);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what is your first name");
+        String userFirstName = sc.next();
+
+        System.out.printf("Nice to meet you , %s",userFirstName);
+
+        System.out.println("How old are you?");
+        int age = sc.nextInt();
+
+
+        System.out.printf("Good to hear you are %d years old.", age);
+
+        System.out.println("Favorite Quote:");
+        String favoriteQuote = sc.nextLine();
+        System.out.printf(" funny, \"%s\" is my favorite quote too.", favoriteQuote);
+
+
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,85 @@ public class HelloWorld {
     public static void main(String[] args ){
 
         System.out.println("Hello world! This is the MARCO CLASS !!!");
+
+        int myFavoriteNumber = 7;
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Im a String";
+
+        System.out.println(myString);
+
+//        String myString = 'C';
+
+//        double 3.14159 = myString;
+//
+//        long myNumber;
+//        System.out.println(myNumber);
+//
+//        int myNumber = 3.14;
+
+//        myNumber =  123L;
+
+//        int myNumber = 123;
+//
+//        double okNo = 3.14;
+
+//        float myNumber = 3.14F;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+//
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int z = 3;
+//        int q = 4;
+//        q *= z;
+
+//        int s = 10;
+//        int a =2;
+//        s/= a;
+//        s -= a;
+//
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+
+        byte x = 127;
+        x ++;
+        System.out.println(x);
+
+
+
+
+
+//        String name = "codeup";
+//        System.out.printf("Hello there, %s. Nice to see you.\n", name);
+
+
+//
+
+
+
+
+
+
     }
 
 
