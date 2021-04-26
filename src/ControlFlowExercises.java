17d0630eb21357e31fadfb28ba8493371e520b05
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-04-26 14:47:21 -0500

    dowhile

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,54 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 
-        int i = 5;
-        while (i <= 15){
-            System.out.println("i is " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+
+
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i-=5;
+//        }
+//        while (i >= -10);
+
+
+
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i*=i;
+//        }
+//        while (i < 100000);
+//
+//
+
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.println(i);
+//        }
+
+
+//        for (long i = 2; i < 1000000; i*=i) {
+//            System.out.println(i);
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
