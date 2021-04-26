c406de96153b0444d5b4f200ecb02f78b7c18af7
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-04-26 10:08:39 -0500

    console ex

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ConsoleExercises {
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args){
+
+        System.out.println("Console IO ");
+
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f.%n " , pi );
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a integer");
+        int num = sc.nextInt();
+
+        System.out.println("enter 3 words");
+        String words = "";
+
+
+
+
+    }
 }
