e70253bbfe5d755b9fbbe4975336b1d262be50f3
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-05-03 09:51:22 -0500

    monday oop

--- src/Person.java
+++ src/Person.java
@@ -1,20 +1,39 @@
 public class Person {
     private String name;
 
-    public String getName(){
-//TODO: return the person's name
-        return name;
-
-    }
-
-    public void setName(String name){
-//TODO: change the name property to the passed value
+    public Person(String name) {
         this.name = name;
-
-    }
-    public void sayHello(){
-//TODO: print a message to the console using the person's name
-
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String sayHello() {
+        return String.format("Hello, my name is %s!", name);
+    }
+
+    public static void main(String[] args) {
+        Person me = new Person("Victor");
+        String newName = "Mono";
+
+        System.out.printf("%s\n", me.getName());
+        System.out.printf("Setting name to %s...\n", newName);
+        me.setName(newName);
+        System.out.printf("%s\n", me.sayHello());
+
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
+    }
 }
---
+++ src/util/Input.java
@@ -0,0 +1,21 @@
+package util;
+import java.util.Scanner;
+
+
+public class Input {
+    private Scanner scanner;
+
+    public  Input() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        String response;
+
+        response = scanner.nextLine();
+
+        return response;
+    }
+
+
+}
